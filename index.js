f835da31f1259d215282f88c532915ede5bb726b
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-04 23:56:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
-  
+  for (let i = 1; i <= katzDeliLine.length; i++) {
+    if (katzDeliLine[i] === name) {
+      return i
+    }
+  }
+}
+
+function nowServing(katzDeli) {
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return katzDeli.slice(1)
+  }
+}
+
+function currentLine(katzDeli) {
+  let counter = 0
+  if (katzDeli.length === 0) {
+    return "The line is currently empty."
+  } else {
+    
+  }
 }
