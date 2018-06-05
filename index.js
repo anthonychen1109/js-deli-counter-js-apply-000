5093fa6a460a9ce5f274d8dac57770c7096cbc2f
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-05 00:23:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name)
+  katzDeliLine.push(name);
   for (let i = 0; i < katzDeliLine.length; i++) {
     if (katzDeliLine[i] === name) {
-      return `Welcome, ${name}. You are number ${i + 1} in line.`
+      return `Welcome, ${name}. You are number ${i + 1} in line.`;
     }
   }
 }
 
 function nowServing(katzDeli) {
   if (katzDeli.length === 0) {
-    return "There is nobody waiting to be served!"
-  } else {
-    return `Currently serving ${katzDeli.shift()}.`
+    return "There is nobody waiting to be served!";
+  } 
+  else {
+    return `Currently serving ${katzDeli.shift()}.`;
   }
 }
 
 function currentLine(katzDeli) {
-  let counter = 0
-  let line = ''
+  let counter = 0;
+  let line = '';
   if (katzDeli.length === 0) {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   } else {
     for(let i=0; i<katzDeli.length; i++) {
-      return `The line is currently: ${i+1},`
+      return `The line is currently: ${i+1} $,`;
     }
   }
 }
